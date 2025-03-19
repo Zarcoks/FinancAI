8d5ea7705d84faec78e04ba1e1d32fd851b68c91
Victor Jost <96876377+Zarcoks@users.noreply.github.com>
2025-03-18 20:58:09 -0400

    Entrainement sur 100 agents pas très concluant

--- test.js
+++ test.js
@@ -9,25 +9,27 @@ const AgentManager = require("./components/Agent/AgentManager")
 const data = DataTranslator.getIBMTable()
 //console.log(DataTranslator.getDays(data))
 
-const calendar = new Calendar(DataTranslator.getDays(data))
+const days = DataTranslator.getDays(data)
+
+const calendar = new Calendar(days)
 const ssm = new SpecificStockMarket("IBM", data, calendar.getCurrentDay())
 
 
-const agent1 = new Agent(new Wallet(200), ssm)
+const agents = []
+for (let k = 0; k<=100; k++) {
+    agents.push(new Agent(new Wallet(200), ssm))
+}
 
 // Training:
-for (let i = 0; i<10000; i++) {
+for (let i = 0; i<days.length-1; i++) {
     //console.log(`Day ${calendar.getCurrentDay()}, in the ${calendar.getIsMorning() ? "morning" : "afternoon"}, price = ${ssm.getCurrentStockPrice()}, average: ${ssm.averageStockPrice}`)
-    if (i%1000 == 0) {
-        console.log(`Hey ! My wallet is currently at: ${agent1.wallet.amount}, totalling: ${agent1.wallet.getTotalAmount()}`)
-        console.log(`The agent state is ${agent1.getState()}`)
-        console.log(agent1.brain.Q)
-    }
     //console.log(agent1.wallet.stockContainerManager.stockContainers[0])
-    AgentManager.applyTrainingDecision(agent1, false)
+    processAgents(agents)
     calendar.nextDayTime()
     ssm.update(calendar.getCurrentDay(), calendar.getIsMorning())
 }
+
+printBestAgentData(agents)
 
 /*
 agent1.wallet = new Wallet(200)
@@ -39,6 +41,35 @@ for (let i = 0; i<100; i++) {
 console.log(`The Agent wallet at the end is ${agent1.wallet.getTotalAmount()}`)
 console.log(agent1.brain.Q)
 */
+/**
+ * 
+ * @param {[]} agents 
+ */
+function processAgents(agents) {
+    agents.forEach((agent) => {
+        AgentManager.applyTrainingDecision(agent, false)
+    })
+}
+
+function printBestAgentData(agents) {
+    let bestScore = agents[0].wallet.getTotalAmount()
+    let bestAgent = agents[0]
+    let averageWallet = 0
+    let totalSeen = 0
+    agents.forEach((agent) => {
+        if (agent.wallet.getTotalAmount() > bestScore) {
+            bestScore = agent.wallet.getTotalAmount()
+            bestAgent = agent
+        }   
+        averageWallet = (averageWallet * totalSeen + agent.wallet.getTotalAmount()) / (totalSeen+1)
+        totalSeen++
+    })
+    console.log(`The average wallet at the end is ${averageWallet}`)
+    console.log("The datas of the best agent")
+    console.log(`Hey ! My wallet is currently at: ${bestAgent.wallet.amount}, totalling: ${bestAgent.wallet.getTotalAmount()}`)
+    console.log(`The agent state is ${bestAgent.getState()}`)
+    console.log(bestAgent.brain.Q)
+}
 
 //let w = new Wallet(100)
 //console.log(w)
